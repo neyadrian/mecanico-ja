b08c37e8eb192d321949784f86b0edc4d638818e
neyadrian <neyadrian2018@gmail.com>
2026-09-06 17:06:47 -0300

    criando entidade PerfilMecanico

---
+++ core-api/src/main/java/com/mecanicoja/core_api/domain/PerfilMecanico.java
@@ -0,0 +1,14 @@
+package com.mecanicoja.core_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "perfis_mecanico")
+public class PerfilMecanico {
+
+}
