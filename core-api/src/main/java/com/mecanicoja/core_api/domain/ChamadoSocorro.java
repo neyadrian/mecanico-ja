9b90552e13b571f12cf9566e4252603fe08d5356
neyadrian <neyadrian2018@gmail.com>
2026-09-07 14:08:54 -0300

    criando entidade ChamadoSocorro

---
+++ core-api/src/main/java/com/mecanicoja/core_api/domain/ChamadoSocorro.java
@@ -0,0 +1,11 @@
+package com.mecanicoja.core_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "chamado_socorro")
+public class ChamadoSocorro {
+}
