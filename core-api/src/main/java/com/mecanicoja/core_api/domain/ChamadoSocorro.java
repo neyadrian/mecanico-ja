74e23ed24e36da55486805b398973931dbe0a562
neyadrian <neyadrian2018@gmail.com>
2026-09-07 14:14:27 -0300

    criando entidade ChamadoSocorro

--- core-api/src/main/java/com/mecanicoja/core_api/domain/ChamadoSocorro.java
+++ core-api/src/main/java/com/mecanicoja/core_api/domain/ChamadoSocorro.java
@@ -1,11 +1,35 @@
 package com.mecanicoja.core_api.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "chamado_socorro")
 public class ChamadoSocorro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "diagnostico_id")
+    private Diagnostico diagnostico;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario motorista;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id",  nullable = false)
+    private Usuario mecanico;
+
+    private Enum status {
+        ENDENTE, ACEITO, RECUSADO, CONCLUIDO, CANCELADO;
+    }
+
+    private LocalDateTime criadoEm;
 }
