2d6ef4ebc87ef083bb52eb22c5b9827fabcbaf43
neyadrian <neyadrian2018@gmail.com>
2026-09-11 14:49:38 -0300

    criando e alimentando VeiculoRepository

---
+++ backend/core-api/src/main/java/com/mecanicoja/core_api/repository/VeiculoRepository.java
@@ -0,0 +1,13 @@
+package com.mecanicoja.core_api.repository;
+
+import com.mecanicoja.core_api.domain.Usuario;
+import com.mecanicoja.core_api.domain.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, UUID> {
+    
+    Optional<Veiculo> findByNome(Veiculo veiculo);
+}
