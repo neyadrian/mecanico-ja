0135912f8c529b6c1d15ae571074c5f64e91adee
neyadrian <neyadrian2018@gmail.com>
2026-09-11 14:38:21 -0300

    alimentando UsuarioRepository

--- backend/core-api/src/main/java/com/mecanicoja/core_api/repository/UsuarioRepository.java
+++ backend/core-api/src/main/java/com/mecanicoja/core_api/repository/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package com.mecanicoja.core_api.repository;
 
-public class UsuarioRepository {
+import com.mecanicoja.core_api.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    // Spring Boot vai criar o SQL sozinho para buscar um usuário pelo email
+    Optional<Usuario> findByEmail(String email);
 }
